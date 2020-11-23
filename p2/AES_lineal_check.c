1c5b9fb8b84c5418270fed8e158fa56c46ba45d2
Estudiante Escuela Politecnica Superior <javier.lopezcano@estudiante.uam.es>
2020-11-23 15:02:02 +0100

    lineal_check_AES

--- p2/AES_lineal_check.c
+++ p2/AES_lineal_check.c
@@ -10,9 +10,9 @@
 #define OK 0
 #define ERR -1
 
-long search_table_AES(long input, int tabla){
-	long fila = input / 16;
-	long columna = input % 16;
+long search_table_AES(int input, int tabla){
+	int fila = input / 16;
+	int columna = input % 16;
 	char *ptr;
 
 	if(tabla == DIRECT){
@@ -22,94 +22,91 @@ long search_table_AES(long input, int tabla){
 	}else{
 		printf("Tabla no válida.");
 		return ERR;
-	}	
+	}
 }
 
 
-int check_table(long input1, long input2, long multiplier, int tabla){
-	long op1, op2, res_sum, res_prod, sum, prod;
+int check_table(int input1, int input2, int tabla){
+	int op1, op2, res_sum, res_prod, sum, prod;
 
-	printf("###################################################################################################\n");
+	printf("######################################################################\n");
 
 	if(tabla == DIRECT){
-		printf("Comprobando no-linealidad tabla directa del AES con sumandos %lx y %lx, y multiplicador %ld:\n", input1, input2, multiplier);
+		printf("Comprobando no-linealidad tabla directa del AES con sumandos %02X y %02X:\n", input1, input2);
 	}else if(tabla == INVERSE){
-		printf("Comprobando no-linealidad tabla inversa del AES con sumandos %lx y %lx, y multiplicador %ld:\n", input1, input2, multiplier);
+		printf("Comprobando no-linealidad tabla inversa del AES con sumandos %02X y %02X:\n", input1, input2);
 	}
 
-	printf("###################################################################################################\n");
+	printf("######################################################################\n");
 
 	op1 = search_table_AES(input1, tabla);
 	if(op1 == ERR) return ERR;
 	op2 = search_table_AES(input2, tabla);
 	if(op2 == ERR) return ERR;
-	printf("f(%lx) = %lx\n", input1, op1);
-	printf("f(%lx) = %lx\n", input2, op2);
-	printf("\nf(%lx) + f(%lx) = %lx\n", input1, input2, (op1 + op2) % 256);
+	printf("f(%02X) = %02X\n", input1, op1);
+	printf("f(%02X) = %02X\n", input2, op2);
+	printf("\nf(%02X) XOR f(%02X) = %02X\n", input1, input2, op1^op2);
 
-	sum = (input1 + input2) % 256;
+	sum = input1^input2;
 	res_sum = search_table_AES(sum, tabla);
 	if(res_sum == ERR) return ERR;
-	printf("f(%lx + %lx) = f(%lx) = %lx\n", input1, input2, sum, res_sum);
-	if(res_sum == op1 + op2){
-		printf("\nCumple linealidad en la suma. f(x) + f(y) = f(x + y)\n\n");
+	printf("f(%02X XOR %02X) = f(%02X) = %02X\n", input1, input2, sum, res_sum);
+	if(res_sum == (op1 ^ op2)){
+		printf("\nCumple linealidad en XOR. f(x) XOR f(y) = f(x XOR y)\n\n");
+		return 100;
 	}else{
-		printf("\nNo cumple lnealidad en la suma. f(x) + f(y) != f(x + y)\n\n");
+		printf("\nNo cumple lnealidad en XOR. f(x) XOR f(y) != f(x XOR y)\n\n");
 	}
-
-	printf("%ld * f(%lx) = %ld * %lx = %lx\n", multiplier, input1, multiplier, op1, (multiplier * op1) % 256);
-	prod = (multiplier * input1) % 256;
-	res_prod = search_table_AES(prod, tabla);
-	if(res_prod == ERR) return ERR;
-	printf("f(%ld * %lx) = f(%lx) = %lx\n", multiplier, input1, prod, res_prod);
-	if(res_prod == multiplier * op1){
-		printf("\nCumple linealidad en el producto. f(a * x) = a * f(x)\n\n");
-	}else{
-		printf("\nNo cumple lnealidad en el producto. f(a * x) != a * f(x)\n\n");
-	}
-	
 	return OK;
 }
 
-int main(int argc, char *argv[]){
-	long input1, input2, multiplier;
-	char *ptr;
-	int tabla, i = 1;
+int main(){
+	int res, i, j, k, l, lineal_dir[300][2], lineal_inv[300][2];
 
-	if(argc != 7){
-		printf("Número incorrecto de arumentos.");
-		return ERR;
+	printf("\n\nPROBANDO TABLA DIRECTA:\n\n");
+	k = 0;
+	for(i=0; i < 256; i++){
+		for(j = 0; j < 256; j++){
+			res = check_table(i, j, DIRECT);
+			if(res == ERR) return ERR;
+			else if(res == 100){
+				lineal_dir[k][0] = i;
+				lineal_dir[k][1] = j;
+				k++;
+			}
+		}
 	}
-    while (i < argc){
-        if (argv[i][0] != '-'){
-            printf("Error: sintaxis\n");
-            return ERR;
-        }else if (strcmp(argv[i], "-D") == 0){
-            tabla = DIRECT;
-        }else if (strcmp(argv[i], "-I") == 0){
-            tabla = INVERSE;
-        }else if (strcmp(argv[i], "-sums") == 0){
-            input1 = strtol(argv[++i], &ptr, 16);
-        	input2 = strtol(argv[++i], &ptr, 16);
-        }else if (strcmp(argv[i], "-m") == 0){
-            multiplier = strtol(argv[++i], &ptr, 16);
-        }else{
-            printf("Error: argumento no identificado\n");
-            return ERR;
-        }
-        if (argc == i){
-            printf("Error: sintaxis\n");
-            return ERR;
-        }else
-            i++;
-    }
 
-    if(input1 > 255 || input2 > 255){
-    	printf("Sumandos no válidos, por favor, introduce solo 2 dígitos hexadecimales.");
-    	return ERR;
-    }
+	printf("\n\nPROBANDO TABLA INVERSA:\n\n");
+	l = 0;
+	for(i=0; i < 256; i++){
+		for(j = 0; j < 256; j++){
+			res = check_table(i, j, INVERSE);
+			if(res == ERR) return ERR;
+			else if(res == 100){
+				lineal_inv[l][0] = i;
+				lineal_inv[l][1] = j;
+				l++;
+			}
+		}
+	}
 
-	if(check_table(input1, input2, multiplier, tabla) == ERR) return ERR;
+	printf("\n##############################################################################");
+	printf("\n\nLa tabla directa cumple linealidad en XOR con los siguientes pares de datos:\n\n");
+	for(i=1; i <= k; i++){
+		printf("\t(%02X, %02X)", lineal_dir[i - 1][0], lineal_dir[i - 1][1]);
+		if(i % 6 == 0) printf("\n");
+	}
+	printf("\n##############################################################################");
+	printf("\n\nLa tabla inversa cumple linealidad en XOR con los siguientes pares de datos:\n\n");
+	for(i=1; i <= l; i++){
+		printf("\t(%02X, %02X)", lineal_inv[i - 1][0], lineal_inv[i - 1][1]);
+		if(i % 6 == 0) printf("\n");
+	}
+
+	printf("\n\nEsto supone que:\n\n");
+	printf("\tLa tabla directa es lineal en XOR para %d pares de 65536 posibles, un %lf%%\n", k, ((double)k/65536) * 100);
+	printf("\tLa tabla inversa es lineal en XOR para %d pares de 65536 posibles, un %lf%%\n", l, ((double)l/65536) * 100);
 
 	return OK;
 }
